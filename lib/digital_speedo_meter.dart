fccae4ce2dafc56c8aecd26f1f84fdf802bf0cc9
Talha137m <muhammadtalha137m@gmail.com>
2022-10-23 03:04:32 +0500

    change

---
+++ lib/digital_speedo_meter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpeeDoMeterPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double width = size.width;
+    final double height = size.height;
+  }
+
+  @override
+  bool shouldRepaint(SpeeDoMeterPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(SpeeDoMeterPainter oldDelegate) => false;
+}
